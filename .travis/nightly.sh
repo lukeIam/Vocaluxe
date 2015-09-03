a8235b5ad5da4ee36c84e9b4b5a682f06cb91920
lukeIam <mail2skywalker@gmail.com>
2015-09-03 17:01:28 +0200

    Travis test 6

---
+++ .travis/nightly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "$TRAVIS_BRANCH" = "travisTest" ]]; then
+	chmod 600 .travis/travisKey;
+	ssh-add .travis/travisKey;
+	git config --global user.email "builds@travis-ci.com";
+	git config --global user.name "Travis CI";
+	export GIT_TAG=Nightly;
+	git tag "$GIT_TAG" -a -m "Generated tag from TravisCI for the latest nightly build";
+	git push -q -f https://github.com/lukeIam/Vocaluxe Nightly;
+fi

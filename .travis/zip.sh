39b50dd50937c19019771db79ef958fcfeb78fbd
lukeIam <mail2skywalker@gmail.com>
2015-09-04 18:30:24 +0200

    Travis test 20

--- .travis/zip.sh
+++ .travis/zip.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ev
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
-	printf 'path: %s\n' "${PWD##*/}";
 	mkdir build;
-	zip -r build/Vocaluxe_Linux__x64_Nightly.zip Output/*;
+	zip -r build/Vocaluxe_Linux_x64_Nightly.zip Output/*;
 fi

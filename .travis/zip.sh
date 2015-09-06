8350d8ab2ee4db617ec51310a111e1408b1d2b7c
lukeIam <mail2skywalker@gmail.com>
2015-09-06 19:21:18 +0200

    Travis test 24

--- .travis/zip.sh
+++ .travis/zip.sh
@@ -2,7 +2,7 @@
 set -ev
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
 	mkdir build;
-	zip -r build/Vocaluxe_Linux_x64_Nightly.zip Output/*;
+	zip -r "build/Vocaluxe_$config_$platform_Nightly.zip" Output/*;
 	ls -l .;
 	ls -l build;
 fi

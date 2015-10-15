444aeadd3b1f174c5dd1b6987358c526898a0369
lukeIam <mail2skywalker@gmail.com>
2015-10-15 16:50:03 +0200

    Try an other origin

--- .travis/pinFFmpegPackage.sh
+++ .travis/pinFFmpegPackage.sh
@@ -4,6 +4,6 @@ set -ev
 sudo mkdir -p /etc/apt/preferences.d/ 
 sudo touch /etc/apt/preferences.d/ffmpeg
 sudo bash -c 'echo "Package: ffmpeg" >> /etc/apt/preferences.d/ffmpeg'
-sudo bash -c 'echo "Pin: origin ppa.launchpad.net" >> /etc/apt/preferences.d/ffmpeg'
+sudo bash -c 'echo "Pin: origin LP-PPA-pavlyshko-precise" >> /etc/apt/preferences.d/ffmpeg'
 sudo bash -c 'echo "Pin-Priority: 1000" >> /etc/apt/preferences.d/ffmpeg'
 sudo bash -c 'echo "" >> /etc/apt/preferences.d/ffmpeg'

76a4ae036d9c95cea6932e25af3456cc682d09e6
lukeIam <mail2skywalker@gmail.com>
2015-10-19 16:33:37 +0200

    Cleanup

--- .travis/pinFFmpegPackage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-
-sudo mkdir -p /etc/apt/preferences.d/ 
-sudo touch /etc/apt/preferences.d/ffmpeg
-sudo bash -c 'echo "Package: ffmpeg" >> /etc/apt/preferences.d/ffmpeg'
-sudo bash -c 'echo "Pin: origin LP-PPA-pavlyshko-precise" >> /etc/apt/preferences.d/ffmpeg'
-sudo bash -c 'echo "Pin-Priority: 1000" >> /etc/apt/preferences.d/ffmpeg'
-sudo bash -c 'echo "" >> /etc/apt/preferences.d/ffmpeg'

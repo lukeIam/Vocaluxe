d60cc955ffa11eb7190be5b271484081b8f9f3d1
lukeIam <2lukeiam@gmail.com>
2017-01-03 12:52:53 +0100

    Also remove the v option from other tar commands

--- .travis/installFFmpeg.sh
+++ .travis/installFFmpeg.sh
@@ -12,7 +12,7 @@ mkdir ~/ffmpeg_sources
 
 cd ~/ffmpeg_sources
 wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-tar xzvf yasm-1.3.0.tar.gz
+tar xzf yasm-1.3.0.tar.gz
 cd yasm-1.3.0
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
 make
@@ -21,7 +21,7 @@ make distclean
 
 cd ~/ffmpeg_sources
 wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
-tar xjvf last_x264.tar.bz2
+tar xjf last_x264.tar.bz2
 cd x264-snapshot*
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --disable-opencl
 PATH="$HOME/bin:$PATH" make
@@ -30,7 +30,7 @@ make distclean
 
 cd ~/ffmpeg_sources
 wget -O fdk-aac.tar.gz https://github.com/mstorsjo/fdk-aac/tarball/master
-tar xzvf fdk-aac.tar.gz
+tar xzf fdk-aac.tar.gz
 cd mstorsjo-fdk-aac*
 autoreconf -fiv
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
@@ -41,7 +41,7 @@ make distclean
 sudo apt-get install nasm
 cd ~/ffmpeg_sources
 wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-tar xzvf lame-3.99.5.tar.gz
+tar xzf lame-3.99.5.tar.gz
 cd lame-3.99.5
 ./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --disable-shared
 make
@@ -50,7 +50,7 @@ make distclean
 
 cd ~/ffmpeg_sources
 wget http://downloads.xiph.org/releases/opus/opus-1.1.2.tar.gz
-tar xzvf opus-1.1.2.tar.gz
+tar xzf opus-1.1.2.tar.gz
 cd opus-1.1.2
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
 make
@@ -59,7 +59,7 @@ make clean
 
 cd ~/ffmpeg_sources
 wget http://storage.googleapis.com/downloads.webmproject.org/releases/webm/libvpx-1.5.0.tar.bz2
-tar xjvf libvpx-1.5.0.tar.bz2
+tar xjf libvpx-1.5.0.tar.bz2
 cd libvpx-1.5.0
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests
 PATH="$HOME/bin:$PATH" make

3fffb3ab6ee5112eab3eae22f35cdcbde4900abf
lukeIam <2lukeiam@gmail.com>
2017-01-05 17:32:09 +0100

    git decribe based on Vocaluxe/Vocaluxe

--- .travis/gitDescribe.sh
+++ .travis/gitDescribe.sh
@@ -4,7 +4,7 @@ set -ev
 my_dir="$(dirname "$0")";
 
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
-	export VersionTag=$(mono "$my_dir/githubDescribe.exe" "lukeIam" "Vocaluxe" "$TRAVIS_COMMIT" "$GITHUB_KEY");
+	export VersionTag=$(mono "$my_dir/githubDescribe.exe" "Vocaluxe" "Vocaluxe" "$TRAVIS_COMMIT" "$GITHUB_KEY");
 else
     export VersionTag="0.0.0-na-PR_$TRAVIS_PULL_REQUEST_SHA"
 fi
